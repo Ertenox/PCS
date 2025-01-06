07e5e460dbb86b0b07eacc673d356bee0cc4463d
Ertenox <auguste.mendy@etu.imt-nord-europe.fr>
2025-01-06 16:23:05 +0100

    Add -Compilation maven/gradle sans TU -Création image docker -Run de l’image sur un serveur de la VM

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
-FROM python:3.11-slim
-# Définition du répertoire de travail 
+# Étape 1 : Utilisation de l'image de base OpenJDK
+FROM openjdk:17-jdk-alpine
+
+# Étape 2 : Définition du répertoire de travail
 WORKDIR /app
 
-COPY ./Tuto-Web-service /app
-RUN pip install --no-cache-dir -r requirements.txt
+# Étape 3 : Copie du fichier JAR pré-compilé
+COPY ./Tuto-Web-service/target/tuto-0.0.1-SNAPSHOT.jar /app/tuto-0.0.1-SNAPSHOT.jar
 
-# Spécifier la commande par défaut
-CMD ["python", "app.py"]
+# Étape 4 : Définition de la commande d'exécution
+CMD ["java", "-jar", "tuto-0.0.1-SNAPSHOT.jar"]
--- main.py
+++ main.py
@@ -15,11 +15,45 @@ def run_maven():
         print(result.stderr)
         exit(result.returncode)
 
+import subprocess
+
+def run_docker():
+    """Lance le conteneur Docker."""
+    print("Création de l'image docker...")
+    result = subprocess.run(["docker", "build", "-t", "app", "."], capture_output=True, text=True, shell=True)
+    if result.returncode == 0:
+        print("Docker : Image créée avec succès !")
+
+        # Lancement du conteneur Docker
+        print("Lancement du conteneur...")
+        run_result = subprocess.run(
+            ["docker", "run","-p","8080:8080", "-d", "--name", "app_container", "app"],
+            capture_output=True,
+            text=True,
+            shell=True
+        )
+        if run_result.returncode == 0:
+            print("Docker : Conteneur lancé avec succès !")
+            print("ID du conteneur :", run_result.stdout.strip())
+        else:
+            print("Docker : Erreur lors du lancement du conteneur.")
+            print(run_result.stdout)
+            print(run_result.stderr)
+            exit(run_result.returncode)
+    else:
+        print("Docker : Erreur lors de la création de l'image.")
+        print(result.stdout)
+        print(result.stderr)
+        exit(result.returncode)
+
+
 
 if __name__ == "__main__":
     #shutil.rmtree("Tuto-Web-service", ignore_errors=True)
-    git.Git().clone("https://github.com/DavidIMT/Tuto-Web-service.git")
+    #git.Git().clone("https://github.com/DavidIMT/Tuto-Web-service.git")
     os.chdir("Tuto-Web-service")
     run_maven()
+    os.chdir("..")
+    run_docker()
 
 
