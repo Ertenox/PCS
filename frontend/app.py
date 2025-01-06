05f83f009fb5abfdaf4d3a2baddd52918f507969
Phoquiche <dustybertin@gmail.com>
2025-01-06 14:12:18 +0100

    Ajout de la partie OAuth Github sur le frontend

--- frontend/app.py
+++ frontend/app.py
@@ -1,8 +1,60 @@
-from flask import Flask, request, jsonify
+from flask import Flask, redirect, url_for, session, request, jsonify
+from flask_oauthlib.client import OAuth
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+
+oauth = OAuth(app)
+github = oauth.remote_app(
+    'github',
+    consumer_key=os.getenv("GITHUB_CLIENT_ID"),
+    consumer_secret=os.getenv("GITHUB_CLIENT_SECRET"),
+    request_token_params={
+        'scope': 'user:email',
+    },
+    base_url='https://api.github.com/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://github.com/login/oauth/access_token',
+    authorize_url='https://github.com/login/oauth/authorize',
+)
+
+# Routes
+@app.route('/')
+def index():
+    if 'github_token' in session:
+        user_info = github.get('user')
+        return jsonify(user_info.data)
+    return '<a href="/login">Login with GitHub</a>'
+
+@app.route('/login')
+def login():
+    return github.authorize(callback="http://127.0.0.1:5000/callback")
+
+@app.route('/logout')
+def logout():
+    session.pop('github_token', None)
+    return redirect(url_for('index'))
+
+@app.route('/callback')
+def authorized():
+    response = github.authorized_response()
+    if response is None or 'access_token' not in response:
+        return 'Access denied: reason={} error={}'.format(
+            request.args.get('error_reason'),
+            request.args.get('error_description')
+        )
+    session['github_token'] = (response['access_token'], '')
+    user_info = github.get('user')
+    return jsonify(user_info.data)
+
+@github.tokengetter
+def get_github_oauth_token():
+    return session.get('github_token')
 
 @app.route('/process', methods=['GET'])
 def process_data():
